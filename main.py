891771504f2305716ae7b9ab65aa998fd8f52b65
69th-zeus <ayush5632roxx@gmail.com>
2025-07-06 23:33:26 +0530

    JEE Page Fully Made

--- main.py
+++ main.py
@@ -1,61 +1,82 @@
-import numpy as np
 import pandas as pd
 import streamlit as st
-from data import *
+from math import ceil
 
 st.set_page_config(layout="wide")
-st.title('JEE College Predictor')
-
 #load data frame
 df = pd.read_csv('result.csv')
 
-#Select variables from the user
-exam = st.selectbox('Select Exam (JEE Mains or JEE Advanced)', EXAM, placeholder='Choose An Option')
+JEE_MAINS = df[df['Exam'] == 'JEE Mains']['Institute'].unique().tolist()
+JEE_MAINS.sort()
+JEE_MAINS.insert(0, 'Show Me All')
 
-quota = st.selectbox('Select Your Quota (You May Skip)', QUOTA)
+JEE_ADV = df[df['Exam'] == 'JEE Advanced']['Institute'].unique().tolist()
+JEE_ADV.sort()
+JEE_ADV.insert(0, 'Show Me All')
 
-rank = st.number_input('Enter Rank', value = 0, step = 1)
-relaxation = st.number_input('Enter Relaxtion (Up what Percent out of your rank would you want the results to be)', value = 5.00, step = 0.001)
-relaxed_rank = rank * (1 - relaxation)
+EXAM = ['JEE Mains', 'JEE Advanced']
+GENDER = ['Show Me All', 'Gender-Neutral', 'Female-only', 'Female-only (including Supernumerary)']
+TYPE = ['Show Me All', 'IIT','NIT', 'IIIT', 'GFTI']
+SIDEBAR = ['JEE', 'NEET']
+YEARS = [2024, 2023, 2022, 2021]
+SEAT_TYPE = [ 'Show Me All', 'EWS', 'OBC-NCL', 'OPEN', 'OPEN (PwD)', 'SC', 'ST', 'OBC-NCL (PwD)', 'EWS (PwD)', 'ST (PwD)', 'SC (PwD)']
+QUOTA = ['Show Me All', 'AI', 'HS', 'OS', 'GO', 'JK', 'LA']
 
-gender = st.selectbox('Select Gender Type of Seats You would like to see (You May Skip)', GENDER)
+def main():
+    st.sidebar.title("Select Your Browsing Method")
+    mode = st.sidebar.selectbox('Select Mode',SIDEBAR, placeholder = 'Choose an Option', label_visibility="collapsed")
 
-institute_type = st.selectbox('Select The type Of Institute You Would Like to See', TYPE)
+    if mode == 'JEE':
+        jee()
+    elif mode == 'Rank Based Recommendation':
+        # neet()
+        pass
 
-temp = df[df['Exam'] == exam]
-if quota != 'Show Me All':
-    temp = temp[temp['Seat Type'] == quota]
-if gender != 'Show Me All':
-    temp = temp[temp['Gender'] == gender]
-if institute_type != 'Show Me All':
-    temp = temp[temp['Type'] == institute_type]
+def jee():
+    st.title('JEE College Recommender')
 
-temp = temp[pd.to_numeric(temp['Closing Rank (Last Round)'], errors='coerce') > rank]
-temp.sort_values(by = 'Closing Rank (Last Round)', inplace=True)
+    # Initialize Page
+    st.title("🎓 Browse Colleges")
 
-if temp.shape[0] == 0:
-    st.error("The Selected Variation has no Seats to Display")
-else:
-    st.dataframe(temp, use_container_width=True)
-    
+    st.write('The Looking Around page lets users explore available colleges based on their preferences. You can select the exam type (JEE Mains or Advanced), quota, gender, and institute type to filter results. It also allows you to choose specific years to view past cutoffs. The results are displayed in organized tables for each selected year. This feature helps users understand seat distributions without needing to input a rank.')
+
+    # Get Variables
+    exam = st.selectbox('Select Exam (JEE Mains or JEE Advanced)', EXAM, placeholder='Choose An Option')
+    seat_type = st.selectbox('Select Your Seat Type (You May Skip)', SEAT_TYPE)
+    gender = st.selectbox('Select Gender Type of Seats You would like to see (You May Skip)', GENDER)
+    institute_type = st.selectbox('Select The type Of Institute You Would Like to See', TYPE)
+    quota = st.selectbox('Select Quota of Seat to be Shown', QUOTA, placeholder='Show Me All')
+    if exam == 'JEE Mains':
+        institute_name = st.selectbox('Select College To View Data (You May Skip)', JEE_MAINS,placeholder='Show Me All')
+    elif exam == 'JEE Advanced':
+        institute_name = st.selectbox('Select College To View Data (You May Skip)', JEE_ADV,placeholder='Show Me All')
+    rank = st.number_input('Enter Your Rank (You May Skip by Entering 0)', min_value=0, step=1, placeholder=0)
+    cutoff = st.number_input("Cutoff threshold (% of your rank) to widen or narrow the seats which you 'may' get.", min_value=0, step = 1, placeholder=10)
+    st.write("Show Data For - ")
+    year_checks = {year: st.checkbox(str(year)) for year in YEARS}
+
+    #Process Data To Show
+    temp = df[df['Exam'] == exam]
+    if seat_type != 'Show Me All':
+        temp = temp[temp['Seat Type'] == seat_type]
+    if gender != 'Show Me All':
+        temp = temp[temp['Gender'] == gender]
+    if institute_type != 'Show Me All':
+        temp = temp[temp['Type'] == institute_type]
+    if quota != 'Show Me All':
+        temp = temp[temp['Quota'] == quota]
+    if rank != 0:
+        temp = temp[temp['Closing Rank (Last Round)'] > (1 + ceil(cutoff/100) * rank)]
+        temp.sort_values(by = 'Opening Rank (First Round)')
+
+    if temp.shape[0] == 0:
+        st.error("The Selected Variation has no Seats to Display")
+    else:
+        st.write("Below are the highly proable seats")
+        for year, checked in year_checks.items():
+            if checked:
+                st.title(f"{year}'s Data")
+                st.dataframe(temp[temp['Year'] == year].reset_index(drop=True), use_container_width=True)
 
 
-# #get all the category and gender divisions
-# category = list(df[df['Institute'].str.contains('Indian Institute of Technology')]['Seat Type'].value_counts().index)
-# selected_category = st.selectbox("Select Category Of Seat To be Shown", category)
-
-# gender = list(df[df['Institute'].str.contains('Indian Institute of Technology')]['Gender'].value_counts().index)
-# selected_gender = st.selectbox("Select Gender Category Of Seat To be Shown", gender)
-
-# rank = st.number_input('Enter your JEE Advanced Rank')
-# data_points= st.number_input('Enter number of Considerable Options you would like to see (Above 100 Ideally)', value = 100, min_value= 100, max_value= 99999, step = 1)
-
-# # process data and show results
-# temp = df[(df['Institute'].str.contains('Indian Institute of Technology'))]
-# temp['Closing Rank'] = pd.to_numeric(temp['Closing Rank'], errors='coerce')
-# temp.dropna(subset=['Closing Rank'], inplace=True)
-# temp = temp[temp['Seat Type'] == selected_category]
-# temp = temp[temp['Gender'] == selected_gender]
-# temp = temp[temp['Closing Rank'] > 0.95 * rank]
-# temp = temp.sort_values(by = 'Closing Rank')
-# st.dataframe(temp.head(data_points))
+main()
